6534d130556e19e6a078d6a777bb03deb11e2bbc
Davit Nazarov <davit_nazarov_github@mail.ru>
2025-07-06 15:00:50 +0400

    sign up the user

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,17 @@ import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
-dotenv.config();
-const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World1!");
-});
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${PORT}`);
 });
